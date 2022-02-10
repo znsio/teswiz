8cddcd4095880722b301f642911a6a01cb2f01f0
Anand Bagmar <abagmar@gmail.com>
2022-02-10 16:30:30 +0530

    add env vars and system props to reportportal when test starts

--- src/main/java/com/znsio/e2e/steps/Hooks.java
+++ src/main/java/com/znsio/e2e/steps/Hooks.java
@@ -24,6 +24,22 @@ public class Hooks {
         SoftAssertions softly = new SoftAssertions();
         testExecutionContext.addTestState(TEST_CONTEXT.SOFT_ASSERTIONS, softly);
         ReportPortal.emitLog(testExecutionContext.getTestState(TEST_CONTEXT.DEVICE_INFO).toString(), "info", new Date());
+        addEnvironmentVariablesToReportPortal();
+        addSystemPropertiesToReportPortal();
+    }
+
+    private void addSystemPropertiesToReportPortal() {
+        Properties props = System.getProperties();
+        final String[] propVars = {""};
+        props.forEach((k, v) -> propVars[0] +=("\t" + k + ":" + v + "\n"));
+        ReportPortal.emitLog("System Properties:\n" + propVars[0], "debug", new Date());
+    }
+
+    private void addEnvironmentVariablesToReportPortal() {
+        Map<String, String> env = System.getenv();
+        final String[] envVars = {""};
+        env.forEach((k, v) -> envVars[0] +=("\t" + k + ":" + v + "\n"));
+        ReportPortal.emitLog("Environment Variables:\n" + envVars[0], "debug", new Date());
     }
 
     public void afterScenario(Scenario scenario) {
