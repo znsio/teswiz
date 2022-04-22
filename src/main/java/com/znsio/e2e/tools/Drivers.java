da5140b408f54336c7e9bbdfac55b93d6bb55277
ritul.mishra <ritul.mishra@ril.com>
2022-04-22 15:34:00 +0530

    -- Integrated Safari browser support for webapp. -- created method createSafariDriver for safari browser

--- src/main/java/com/znsio/e2e/tools/Drivers.java
+++ src/main/java/com/znsio/e2e/tools/Drivers.java
@@ -22,6 +22,8 @@ import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.firefox.*;
 import org.openqa.selenium.logging.*;
 import org.openqa.selenium.remote.*;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 
 import java.io.*;
 import java.net.*;
@@ -319,11 +321,13 @@ public class Drivers {
             case FIREFOX:
                 driver = createFirefoxDriver(forUserPersona, testExecutionContext, browserConfig.getJSONObject(driverManagerType.getBrowserNameLowerCase()));
                 break;
-            case OPERA:
+            case SAFARI:
+                driver = createSafariDriver(forUserPersona, testExecutionContext, browserConfig.getJSONObject(driverManagerType.getBrowserNameLowerCase()));
+                break;
             case EDGE:
             case IEXPLORER:
             case CHROMIUM:
-            case SAFARI:
+            case OPERA:
                 throw new InvalidTestDataException(String.format("Browser: '%s' is NOT supported", browserType));
         }
         LOGGER.info("Driver created");
@@ -336,6 +340,19 @@ public class Drivers {
             driver.manage().window().setSize(new Dimension(1920, 1080));
         }
         LOGGER.info("Reset browser window size");
+        return driver;
+    }
+
+    private WebDriver createSafariDriver(String forUserPersona, TestExecutionContext testExecutionContext, JSONObject safariConfigurations) {
+        SafariOptions safariOptions = new SafariOptions();
+        shouldBrowserBeMaximized = safariConfigurations.getBoolean("maximize");
+        LOGGER.info("SafariOptions: " + safariOptions.asMap());
+        setLogFileName(forUserPersona, testExecutionContext, "Safari");
+        WebDriver driver = Runner.isRunningInCI() ? createRemoteWebDriver(safariOptions) : new SafariDriver(safariOptions);
+        LOGGER.info("Safari driver created");
+        Capabilities capabilities = Runner.isRunningInCI() ? ((RemoteWebDriver) driver).getCapabilities() : ((SafariDriver) driver).getCapabilities();
+        userPersonaDriverCapabilities.put(forUserPersona, capabilities);
+        LOGGER.info("Safari driver capabilities extracted for further use");
         return driver;
     }
 
