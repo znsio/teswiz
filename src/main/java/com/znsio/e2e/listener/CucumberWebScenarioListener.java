6fce4af3e97aa1eb167080b840490a29179ce10f
Anand Bagmar <abagmar@gmail.com>
2022-06-09 12:50:23 +0530

    reindented listeners

--- src/main/java/com/znsio/e2e/listener/CucumberWebScenarioListener.java
+++ src/main/java/com/znsio/e2e/listener/CucumberWebScenarioListener.java
@@ -1,24 +1,27 @@
 package com.znsio.e2e.listener;
 
-import com.appium.filelocations.*;
-import com.context.*;
-import com.znsio.e2e.entities.*;
-import com.znsio.e2e.runner.*;
-import io.cucumber.plugin.*;
+import com.appium.filelocations.FileLocations;
+import com.context.SessionContext;
+import com.context.TestExecutionContext;
+import com.znsio.e2e.entities.TEST_CONTEXT;
+import com.znsio.e2e.runner.Runner;
+import io.cucumber.plugin.ConcurrentEventListener;
 import io.cucumber.plugin.event.*;
-import org.apache.log4j.*;
+import org.apache.log4j.Logger;
 
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 
-public class CucumberWebScenarioListener implements ConcurrentEventListener {
+public class CucumberWebScenarioListener
+        implements ConcurrentEventListener {
     private static final Logger LOGGER = Logger.getLogger(CucumberWebScenarioListener.class.getName());
     private final Map<String, Integer> scenarioRunCounts = new HashMap<>();
 
     public CucumberWebScenarioListener() {
-        LOGGER.info(String.format("ThreadID: %d: CucumberWebScenarioListener\n",
-                Thread.currentThread().getId()));
+        LOGGER.info(String.format("ThreadID: %d: CucumberWebScenarioListener\n", Thread.currentThread()
+                                                                                       .getId()));
     }
 
     @Override
@@ -31,51 +34,53 @@ public class CucumberWebScenarioListener implements ConcurrentEventListener {
 
     private void webRunStartedHandler(TestRunStarted event) {
         LOGGER.info("webRunStartedHandler");
-        LOGGER.info(String.format("ThreadID: %d: beforeSuite: \n", Thread.currentThread().getId()));
+        LOGGER.info(String.format("ThreadID: %d: beforeSuite: \n", Thread.currentThread()
+                                                                         .getId()));
     }
 
     private void webCaseStartedHandler(TestCaseStarted event) {
-        String scenarioName = event.getTestCase().getName();
+        String scenarioName = event.getTestCase()
+                                   .getName();
         TestExecutionContext testExecutionContext = new TestExecutionContext(scenarioName);
 
-        LOGGER.info("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   TEST-CASE  -- " + scenarioName + "  STARTED   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        LOGGER.info(
+                "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   TEST-CASE  -- " + scenarioName + "  STARTED   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
         LOGGER.info("webCaseStartedHandler: " + scenarioName);
         Integer scenarioRunCount = getScenarioRunCount(scenarioName);
         String normalisedScenarioName = normaliseScenarioName(scenarioName);
 
-        LOGGER.info(
-                String.format("ThreadID: %d: beforeScenario: for scenario: %s\n",
-                        Thread.currentThread().getId(), scenarioName));
-        testExecutionContext.addTestState(TEST_CONTEXT.SCENARIO_LOG_DIRECTORY, FileLocations.REPORTS_DIRECTORY
-                                                                                       + normalisedScenarioName);
+        LOGGER.info(String.format("ThreadID: %d: beforeScenario: for scenario: %s\n", Thread.currentThread()
+                                                                                            .getId(), scenarioName));
+        testExecutionContext.addTestState(TEST_CONTEXT.SCENARIO_LOG_DIRECTORY, FileLocations.REPORTS_DIRECTORY + normalisedScenarioName);
         testExecutionContext.addTestState(TEST_CONTEXT.SCREENSHOT_DIRECTORY,
-                FileLocations.REPORTS_DIRECTORY
-                        + normalisedScenarioName
-                        + File.separator
-                        + "screenshot"
-                        + File.separator);
+                                          FileLocations.REPORTS_DIRECTORY + normalisedScenarioName + File.separator + "screenshot" + File.separator);
     }
 
     private void webCaseFinishedHandler(TestCaseFinished event) {
-        String scenarioName = event.getTestCase().getName();
+        String scenarioName = event.getTestCase()
+                                   .getName();
         LOGGER.info("webCaseFinishedHandler Name: " + scenarioName);
-        LOGGER.info("webCaseFinishedHandler Result: " + event.getResult().getStatus().toString());
-        long threadId = Thread.currentThread().getId();
-        LOGGER.info(
-                String.format("ThreadID: %d: afterScenario: for scenario: %s\n",
-                        threadId, scenarioName));
+        LOGGER.info("webCaseFinishedHandler Result: " + event.getResult()
+                                                             .getStatus()
+                                                             .toString());
+        long threadId = Thread.currentThread()
+                              .getId();
+        LOGGER.info(String.format("ThreadID: %d: afterScenario: for scenario: %s\n", threadId, scenarioName));
         Runner.remove(threadId);
-        LOGGER.info("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   TEST-CASE  -- " + scenarioName + "  ENDED   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        LOGGER.info(
+                "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   TEST-CASE  -- " + scenarioName + "  ENDED   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
     }
 
     private void webRunFinishedHandler(TestRunFinished event) {
-        LOGGER.info("webRunFinishedHandler: " + event.getResult().toString());
+        LOGGER.info("webRunFinishedHandler: " + event.getResult()
+                                                     .toString());
         SessionContext.setReportPortalLaunchURL();
-        LOGGER.info(String.format("ThreadID: %d: afterSuite: \n", Thread.currentThread().getId()));
+        LOGGER.info(String.format("ThreadID: %d: afterSuite: \n", Thread.currentThread()
+                                                                        .getId()));
     }
 
     private Integer getScenarioRunCount(String scenarioName) {
-        if (scenarioRunCounts.containsKey(scenarioName)) {
+        if(scenarioRunCounts.containsKey(scenarioName)) {
             scenarioRunCounts.put(scenarioName, scenarioRunCounts.get(scenarioName) + 1);
         } else {
             scenarioRunCounts.put(scenarioName, 1);
--- src/main/java/com/znsio/e2e/listener/CucumberWebScenarioReporterListener.java
+++ src/main/java/com/znsio/e2e/listener/CucumberWebScenarioReporterListener.java
@@ -1,13 +1,14 @@
 package com.znsio.e2e.listener;
 
-import com.epam.reportportal.cucumber.*;
-import com.epam.reportportal.utils.*;
-import com.epam.ta.reportportal.ws.model.*;
-import org.apache.log4j.*;
+import com.epam.reportportal.cucumber.ScenarioReporter;
+import com.epam.reportportal.utils.MemoizingSupplier;
+import com.epam.ta.reportportal.ws.model.StartTestItemRQ;
+import org.apache.log4j.Logger;
 
-import java.util.*;
+import java.util.Calendar;
 
-public class CucumberWebScenarioReporterListener extends ScenarioReporter {
+public class CucumberWebScenarioReporterListener
+        extends ScenarioReporter {
     private static final Logger LOGGER = Logger.getLogger(CucumberWebScenarioReporterListener.class.getName());
     private static final String DUMMY_ROOT_SUITE_NAME = "End-2-End Tests";
     private static final String RP_STORY_TYPE = "SUITE";
@@ -18,13 +19,14 @@ public class CucumberWebScenarioReporterListener extends ScenarioReporter {
 
     @Override
     protected void startRootItem() {
-        this.rootSuiteId =
-                rootSuiteId = new MemoizingSupplier<>(() -> {
-                    StartTestItemRQ rq = new StartTestItemRQ();
-                    rq.setName(DUMMY_ROOT_SUITE_NAME);
-                    rq.setStartTime(Calendar.getInstance().getTime());
-                    rq.setType(RP_STORY_TYPE);
-                    return launch.get().startTestItem(rq);
-                });
+        this.rootSuiteId = rootSuiteId = new MemoizingSupplier<>(() -> {
+            StartTestItemRQ rq = new StartTestItemRQ();
+            rq.setName(DUMMY_ROOT_SUITE_NAME);
+            rq.setStartTime(Calendar.getInstance()
+                                    .getTime());
+            rq.setType(RP_STORY_TYPE);
+            return launch.get()
+                         .startTestItem(rq);
+        });
     }
 }
