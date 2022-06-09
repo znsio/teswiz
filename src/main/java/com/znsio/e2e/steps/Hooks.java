44d90388419993739ce20d9b35acc24261777920
Anand Bagmar <abagmar@gmail.com>
2022-06-09 12:58:38 +0530

    reindented steps

--- src/main/java/com/znsio/e2e/steps/Hooks.java
+++ src/main/java/com/znsio/e2e/steps/Hooks.java
@@ -1,15 +1,18 @@
 package com.znsio.e2e.steps;
 
-import com.context.*;
-import com.epam.reportportal.service.*;
-import com.znsio.e2e.entities.*;
-import com.znsio.e2e.runner.*;
-import com.znsio.e2e.tools.*;
-import io.cucumber.java.*;
-import org.apache.log4j.*;
-import org.assertj.core.api.*;
+import com.context.TestExecutionContext;
+import com.epam.reportportal.service.ReportPortal;
+import com.znsio.e2e.entities.TEST_CONTEXT;
+import com.znsio.e2e.runner.Runner;
+import com.znsio.e2e.tools.Drivers;
+import com.znsio.e2e.tools.ScreenShotManager;
+import io.cucumber.java.Scenario;
+import org.apache.log4j.Logger;
+import org.assertj.core.api.SoftAssertions;
 
-import java.util.*;
+import java.util.Date;
+import java.util.Map;
+import java.util.Properties;
 
 public class Hooks {
     private static final Logger LOGGER = Logger.getLogger(Hooks.class.getName());
@@ -17,7 +20,8 @@ public class Hooks {
     private static final String INFO = "info";
 
     public void beforeScenario(Scenario scenario) {
-        long threadId = Thread.currentThread().getId();
+        long threadId = Thread.currentThread()
+                              .getId();
         TestExecutionContext testExecutionContext = Runner.getTestExecutionContext(threadId);
         LOGGER.info(String.format("ThreadId : %s In RunCukes - Before: %s", threadId, scenario.getName()));
         LOGGER.info(String.format("Running test %s on %s", testExecutionContext.getTestName(), Runner.platform.name()));
@@ -29,13 +33,6 @@ public class Hooks {
         addSystemPropertiesToReportPortal();
     }
 
-    private void addSystemPropertiesToReportPortal() {
-        Properties props = System.getProperties();
-        final String[] propVars = {""};
-        props.forEach((k, v) -> propVars[0] += ("\t" + k + ":" + v + "\n"));
-        ReportPortal.emitLog("System Properties:\n" + propVars[0], DEBUG, new Date());
-    }
-
     private void addEnvironmentVariablesToReportPortal() {
         Map<String, String> env = System.getenv();
         final String[] envVars = {""};
@@ -43,8 +40,16 @@ public class Hooks {
         ReportPortal.emitLog("Environment Variables:\n" + envVars[0], DEBUG, new Date());
     }
 
+    private void addSystemPropertiesToReportPortal() {
+        Properties props = System.getProperties();
+        final String[] propVars = {""};
+        props.forEach((k, v) -> propVars[0] += ("\t" + k + ":" + v + "\n"));
+        ReportPortal.emitLog("System Properties:\n" + propVars[0], DEBUG, new Date());
+    }
+
     public void afterScenario(Scenario scenario) {
-        long threadId = Thread.currentThread().getId();
+        long threadId = Thread.currentThread()
+                              .getId();
         LOGGER.info("ThreadId: " + threadId + " In RunCukes - After: " + scenario.getName());
         Runner.closeAllDrivers(threadId);
         SoftAssertions softly = Runner.getSoftAssertion(threadId);
--- src/main/java/com/znsio/e2e/steps/RunCukes.java
+++ src/main/java/com/znsio/e2e/steps/RunCukes.java
@@ -1,17 +1,22 @@
 package com.znsio.e2e.steps;
 
-import com.context.*;
-import io.cucumber.java.*;
-import io.cucumber.testng.*;
-import org.apache.log4j.*;
-import org.testng.annotations.*;
+import com.context.SessionContext;
+import com.context.TestExecutionContext;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import org.apache.log4j.Logger;
+import org.testng.annotations.DataProvider;
 
-public class RunCukes extends AbstractTestNGCucumberTests {
+public class RunCukes
+        extends AbstractTestNGCucumberTests {
     private static final Logger LOGGER = Logger.getLogger(RunCukes.class.getName());
     private final TestExecutionContext context;
 
     public RunCukes() {
-        long threadId = Thread.currentThread().getId();
+        long threadId = Thread.currentThread()
+                              .getId();
         LOGGER.info("RunCukes constructor: ThreadId: " + threadId);
         context = SessionContext.getTestExecutionContext(threadId);
     }
@@ -19,7 +24,8 @@ public class RunCukes extends AbstractTestNGCucumberTests {
     @Override
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
-        LOGGER.info(String.format("ThreadID: %d: in overridden scenarios%n", Thread.currentThread().getId()));
+        LOGGER.info(String.format("ThreadID: %d: in overridden scenarios%n", Thread.currentThread()
+                                                                                   .getId()));
         Object[][] scenarios = super.scenarios();
         LOGGER.info(scenarios);
         return scenarios;
