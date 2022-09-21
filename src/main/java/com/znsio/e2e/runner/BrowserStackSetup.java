cc74abcef3750df4713ae42e197c9c90b441c887
Anand Bagmar <abagmar@gmail.com>
2022-09-21 07:32:01 +0530

    fixed incorrect log message

--- src/main/java/com/znsio/e2e/runner/BrowserStackSetup.java
+++ src/main/java/com/znsio/e2e/runner/BrowserStackSetup.java
@@ -187,7 +187,7 @@ public class BrowserStackSetup {
             if(bsLocal.isRunning()) {
                 LOGGER.info("Stopping BrowserStackLocal");
                 bsLocal.stop();
-                LOGGER.info("Is BrowserStackLocal stopped? - " + bsLocal.isRunning());
+                LOGGER.info("Is BrowserStackLocal stopped? - " + !bsLocal.isRunning());
             }
         } catch(Exception e) {
             throw new RuntimeException("Exception in stopping BrowserStackLocal", e);
