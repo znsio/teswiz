be11a3571f913fdbc63b07f7b2788791e512b96e
Mohammad Shahrukh <mohammadsrk001@gmail.com>
2023-01-03 19:17:10 +0530

    Added VisualTestSetupException

---
+++ src/main/java/com/znsio/e2e/exceptions/VisualTestSetupException.java
@@ -0,0 +1,7 @@
+package com.znsio.e2e.exceptions;
+
+public class VisualTestSetupException extends RuntimeException{
+    public VisualTestSetupException(String message) {
+        super(message);
+    }
+}
