b4335bebd52aa6ce967b64f1f9c34b78fdeb5ae8
Mohammad Shahrukh <mohammadsrk001@gmail.com>
2023-02-02 18:00:29 +0530

    Added ajio test for image search

---
+++ src/test/java/com/znsio/sample/e2e/screen/android/ajio/SearchScreenAndroid.java
@@ -0,0 +1,29 @@
+package com.znsio.sample.e2e.screen.android.ajio;
+
+import com.znsio.e2e.tools.Driver;
+import com.znsio.e2e.tools.Visual;
+import com.znsio.sample.e2e.screen.ajio.SearchScreen;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+
+public class SearchScreenAndroid extends SearchScreen {
+    private final Driver driver;
+    private final Visual visually;
+    private static final String SCREEN_NAME = SearchScreenAndroid.class.getSimpleName();
+    private static final Logger LOGGER = Logger.getLogger(SCREEN_NAME);
+    private static final By byResultsId = By.id("com.ril.ajio:id/tv_count_plp_header_is");
+
+    public SearchScreenAndroid(Driver driver, Visual visually) {
+        this.driver = driver;
+        this.visually = visually;
+    }
+
+    @Override
+    public int numberOfProductFound() {
+
+        int numberOfProductFound = Integer.parseInt(driver.waitTillElementIsPresent(byResultsId).getText().split(" ")[0]);
+        LOGGER.info("numberOfProductFound: "+numberOfProductFound);
+        return numberOfProductFound;
+
+    }
+}
---
+++ src/test/resources/com/znsio/sample/e2e/features/ajio.feature
@@ -0,0 +1,9 @@
+@ajio @prod @guestUser
+Feature: Ajio tests
+
+#  CONFIG=./configs/ajio_local_config.properties TAG="@ajio and @photosearch" PLATFORM=android ./gradlew run
+  @android @single-app @imagesearch
+  Scenario: As a guest user, I should be able to look for a product using image search and  prepare a cart
+	Given  I search for products using "image"
+	When I add the product to the cart
+	Then I should see the product in the cart
