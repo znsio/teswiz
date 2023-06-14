a8bc6e2345f8dd7698f37e48d88cfd240ba201f7
Mukund Gupta <mukundgpt2025@gmail.com>
2023-06-14 11:53:37 +0530

    Created files for VodQA application testing and implemented scroll() method (#179)
    * Created config and capabilities file for VodQA App testing
    
    * Created feature, steps, BL, abstract screen and android screen files for VodQA scroll testing
    
    * VodQA config file path set in CONFIG variable
    
    * Implementation of scroll(fromPoint, toPoint) method according to appium 2.0 changes
    
    * removed config path
    
    * added command to execute feature, changed scenario name
    
    * changed function and arguement names
    
    * merged "When" "And" in feature and its related code in other files
    
    * replaced 700 millisec with 1 second wait
    
    * Removed string passed from feature, no longer needed
    
    * modified steps according to feature
    
    * changed method names and removed arguments
    
    * changed method names, removed arguments, added visual validations and moved locators to top
    
    * changed "Given"
    
    * changed steps according to feature
    
    ---------
    
    Co-authored-by: Mukund1 Gupta <Mukund1.Gupta@ril.com>

---
+++ src/test/java/com/znsio/teswiz/businessLayer/vodqa/VodqaBL.java
@@ -0,0 +1,51 @@
+package com.znsio.teswiz.businessLayer.vodqa;
+
+import com.context.TestExecutionContext;
+import com.znsio.teswiz.entities.Platform;
+import com.znsio.teswiz.entities.SAMPLE_TEST_CONTEXT;
+import com.znsio.teswiz.runner.Runner;
+import com.znsio.teswiz.screen.vodqa.VodqaScreen;
+import org.apache.log4j.Logger;
+import org.assertj.core.api.SoftAssertions;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class VodqaBL {
+    private static final Logger LOGGER = Logger.getLogger(VodqaBL.class.getName());
+    private final TestExecutionContext context;
+    private final SoftAssertions softly;
+    private final String currentUserPersona;
+    private final Platform currentPlatform;
+
+    public VodqaBL(String userPersona, Platform forPlatform) {
+        long threadId = Thread.currentThread().getId();
+        this.context = Runner.getTestExecutionContext(threadId);
+        softly = Runner.getSoftAssertion(threadId);
+        this.currentUserPersona = userPersona;
+        this.currentPlatform = forPlatform;
+        Runner.setCurrentDriverForUser(userPersona, forPlatform, context);
+    }
+
+    public VodqaBL() {
+        long threadId = Thread.currentThread().getId();
+        this.context = Runner.getTestExecutionContext(threadId);
+        softly = Runner.getSoftAssertion(threadId);
+        this.currentUserPersona = SAMPLE_TEST_CONTEXT.ME;
+        this.currentPlatform = Runner.getPlatform();
+    }
+
+    public VodqaBL login() {
+        VodqaScreen.get().login();
+        return this;
+    }
+
+    public VodqaBL scrollFromOneElementPointToAnother() {
+        VodqaScreen.get().scrollFromOneElementPointToAnother();
+        return this;
+    }
+
+    public VodqaBL isElementWithTextVisible() {
+        boolean isScrollSuccessful= VodqaScreen.get().isElementWithTextVisible();
+        assertThat(isScrollSuccessful).as("Scroll was not successful, text is not visible").isTrue();
+        return this;
+    }
+}
---
+++ src/test/java/com/znsio/teswiz/screen/android/vodqa/VodqaScreenAndroid.java
@@ -0,0 +1,51 @@
+package com.znsio.teswiz.screen.android.vodqa;
+
+import com.znsio.teswiz.runner.Driver;
+import com.znsio.teswiz.runner.Visual;
+import com.znsio.teswiz.screen.vodqa.VodqaScreen;
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+
+public class VodqaScreenAndroid extends VodqaScreen {
+    private final Driver driver;
+    private final Visual visually;
+    private final String SCREEN_NAME = VodqaScreenAndroid.class.getSimpleName();
+    private final By byLoginButton = AppiumBy.xpath("//android.view.ViewGroup[@content-desc='login']/android.widget.Button");
+    private final By byVerticalSwipeViewGroup = AppiumBy.xpath("//android.view.ViewGroup[@content-desc='verticalSwipe']");
+    private final By byCLanguageTextView = AppiumBy.xpath("//android.widget.TextView[@text=' C']");
+    private final By byRubyLanguageTextView = AppiumBy.xpath("//android.widget.TextView[@text=' Ruby']");
+    private final By byJasmineLanguageTextView = AppiumBy.xpath("//android.widget.TextView[@text=' Jasmine']");
+
+    public VodqaScreenAndroid(Driver driver, Visual visually) {
+        this.driver = driver;
+        this.visually = visually;
+    }
+
+    @Override
+    public VodqaScreen login() {
+        driver.waitTillElementIsPresent(byLoginButton);
+        visually.checkWindow(SCREEN_NAME, "Login Screen");
+        driver.findElement(byLoginButton).click();
+        return this;
+    }
+
+    @Override
+    public VodqaScreen scrollFromOneElementPointToAnother() {
+        driver.waitTillElementIsPresent(byVerticalSwipeViewGroup);
+        visually.checkWindow(SCREEN_NAME, "Home Screen");
+        driver.findElement(byVerticalSwipeViewGroup).click();
+        driver.waitTillElementIsPresent(byCLanguageTextView);
+        visually.checkWindow(SCREEN_NAME, "Vertical Swiping Screen Before Scroll");
+        Point fromPoint = driver.findElement(byCLanguageTextView).getLocation();
+        Point toPoint = driver.findElement(byRubyLanguageTextView).getLocation();
+        driver.scroll(fromPoint, toPoint);
+        visually.checkWindow(SCREEN_NAME, "Vertical Swiping Screen After Scroll");
+        return this;
+    }
+
+    @Override
+    public boolean isElementWithTextVisible() {
+        return driver.isElementPresent(byJasmineLanguageTextView);
+    }
+}
