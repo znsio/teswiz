432a8a06e934fb39cd2996af455fb59d5b72888d
nayan.gourkar <nayan.gourkar@ril.com>
2023-06-14 12:02:14 +0530

    Latest pull

--- src/test/java/com/znsio/teswiz/steps/VodQaSteps.java
+++ src/test/java/com/znsio/teswiz/steps/VodQaSteps.java
@@ -2,26 +2,39 @@ package com.znsio.teswiz.steps;
 
 import com.context.SessionContext;
 import com.context.TestExecutionContext;
+import com.znsio.teswiz.businessLayer.calculator.CalculatorBL;
 import com.znsio.teswiz.businessLayer.vodqa.VodqaBL;
 import com.znsio.teswiz.entities.SAMPLE_TEST_CONTEXT;
 import com.znsio.teswiz.runner.Drivers;
 import com.znsio.teswiz.runner.Runner;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.apache.log4j.Logger;
 
-
-public class VodQaSteps {
-    private static final Logger LOGGER = Logger.getLogger(TheAppSteps.class.getName());
+public class VodQASteps {
+    private static final Logger LOGGER = Logger.getLogger(VodQASteps.class.getName());
     private final TestExecutionContext context;
 
-    public VodQaSteps() {
-        this.context = SessionContext.getTestExecutionContext(Thread.currentThread().getId());
+    public VodQASteps() {
+        context = SessionContext.getTestExecutionContext(Thread.currentThread().getId());
         LOGGER.info("context: " + context.getTestName());
     }
 
     @Given("I login to vodqa application using valid credentials")
-    public void iLoginToVodqaApplicationUsingValidCredentials() {
+    public void loginToApplication() {
         Drivers.createDriverFor(SAMPLE_TEST_CONTEXT.ME, Runner.getPlatform(), context);
         new VodqaBL(SAMPLE_TEST_CONTEXT.ME, Runner.getPlatform()).login();
+    }
+
+    @When("I scroll from one to another element point on vertical swiping screen")
+    public void scrollToElement() {
+        new VodqaBL(SAMPLE_TEST_CONTEXT.ME, Runner.getPlatform()).scrollFromOneElementPointToAnother();
+    }
+
+    @Then("Element text should be visible")
+    public void isElementWithTextVisible() {
+        new VodqaBL(SAMPLE_TEST_CONTEXT.ME, Runner.getPlatform()).isElementWithTextVisible();
     }
 }
--- src/test/resources/com/znsio/teswiz/features/vodqa.feature
+++ src/test/resources/com/znsio/teswiz/features/vodqa.feature
@@ -7,3 +7,13 @@ Feature: Vodqa test
     Given I login to vodqa application using valid credentials
     When I scroll from one to another element point on vertical swiping screen
     Then Element text should be visible
+
+#  CONFIG=./configs/vodqa_local_config.properties PLATFORM=android TAG=vodqaContextSwitch ./gradlew run
+  @vodqaContextSwitch @android
+  Scenario: Validate context switching between native and web view
+    Given I login to vodqa application using valid credentials
+    When I enter into hacker news under webview section
+    Then I am able to see hacker news login option in web view
+    When I enter into native view section
+    Then I am able to retrieve the native view section items
+
