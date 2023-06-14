7f0c0d2ffe89bb7d3770c2d3e448c8372a106731
medha.gupta <medha.gupta@ril.com>
2023-06-14 13:40:25 +0530

    Revert "removing existing files"
    This reverts commit 44259e6633aa62cec6144918a92cbe907f80d0aa.

---
+++ src/test/java/com/znsio/teswiz/screen/vodqa/VodqaScreen.java
@@ -0,0 +1,35 @@
+package com.znsio.teswiz.screen.vodqa;
+
+import com.znsio.teswiz.entities.Platform;
+import com.znsio.teswiz.runner.Driver;
+import com.znsio.teswiz.runner.Drivers;
+import com.znsio.teswiz.runner.Runner;
+import com.znsio.teswiz.runner.Visual;
+import com.znsio.teswiz.screen.android.vodqa.VodqaScreenAndroid;
+import org.apache.commons.lang3.NotImplementedException;
+import org.apache.log4j.Logger;
+
+public abstract class VodqaScreen {
+    private static final String SCREEN_NAME = VodqaScreen.class.getSimpleName();
+    private static final Logger LOGGER = Logger.getLogger(SCREEN_NAME);
+
+    public static VodqaScreen get() {
+        Driver driver = Drivers.getDriverForCurrentUser(Thread.currentThread().getId());
+        Platform platform = Runner.fetchPlatform(Thread.currentThread().getId());
+        LOGGER.info(SCREEN_NAME + ": Driver type: " + driver.getType() + ": Platform: " + platform);
+        Visual visually = Drivers.getVisualDriverForCurrentUser(Thread.currentThread().getId());
+
+        switch(platform) {
+            case android:
+                return new VodqaScreenAndroid(driver, visually);
+        }
+        throw new NotImplementedException(
+                SCREEN_NAME + " is not implemented in " + Runner.getPlatform());
+    }
+
+    public abstract VodqaScreen login();
+    public abstract VodqaScreen scrollFromOneElementPointToAnother();
+    public abstract boolean isElementWithTextVisible();
+    public abstract VodqaScreen tapInTheMiddle();
+    public abstract boolean isPreviousPageHeaderNotVisible(String landedPage);
+}
---
+++ src/test/java/com/znsio/teswiz/steps/VodQASteps.java
@@ -0,0 +1,47 @@
+package com.znsio.teswiz.steps;
+
+import com.context.SessionContext;
+import com.context.TestExecutionContext;
+import com.znsio.teswiz.businessLayer.vodqa.VodqaBL;
+import com.znsio.teswiz.entities.SAMPLE_TEST_CONTEXT;
+import com.znsio.teswiz.runner.Drivers;
+import com.znsio.teswiz.runner.Runner;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.apache.log4j.Logger;
+
+public class VodQASteps {
+    private static final Logger LOGGER = Logger.getLogger(VodQASteps.class.getName());
+    private final TestExecutionContext context;
+
+    public VodQASteps() {
+        context = SessionContext.getTestExecutionContext(Thread.currentThread().getId());
+        LOGGER.info("context: " + context.getTestName());
+    }
+
+    @Given("I login to vodqa application using valid credentials")
+    public void loginToApplication() {
+        Drivers.createDriverFor(SAMPLE_TEST_CONTEXT.ME, Runner.getPlatform(), context);
+        new VodqaBL(SAMPLE_TEST_CONTEXT.ME, Runner.getPlatform()).login();
+    }
+
+    @When("I scroll from one to another element point on vertical swiping screen")
+    public void scrollToElement() {
+        new VodqaBL(SAMPLE_TEST_CONTEXT.ME, Runner.getPlatform()).scrollFromOneElementPointToAnother();
+    }
+
+    @Then("Element text should be visible")
+    public void isElementWithTextVisible() {
+        new VodqaBL(SAMPLE_TEST_CONTEXT.ME, Runner.getPlatform()).isElementWithTextVisible();
+    }
+    @When("I tap in the middle of the screen")
+    public void iTapInTheMiddleOfTheScreen() {
+        new VodqaBL(SAMPLE_TEST_CONTEXT.ME, Runner.getPlatform()).tapInTheMiddleOfTheScreen();
+    }
+
+    @Then("I am able to move from {string} page to next page")
+    public void iAmAbleToMoveFromPageToNextPage(String pageHeader) {
+        new VodqaBL(SAMPLE_TEST_CONTEXT.ME, Runner.getPlatform()).verifyUserOnNextSection(pageHeader);
+    }
+}
