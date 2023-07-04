82b9d3121d2e6c550cef140ecfd41aa81b632666
saurav.gaur <saurav.gaur@ril.com>
2023-07-04 13:30:21 +0530

    Added TestExecution Context

--- src/test/java/com/znsio/teswiz/steps/ApiTestSteps.java
+++ src/test/java/com/znsio/teswiz/steps/ApiTestSteps.java
@@ -1,12 +1,20 @@
 package com.znsio.teswiz.steps;
-
+import com.context.SessionContext;
+import com.context.TestExecutionContext;
 import com.znsio.teswiz.businessLayer.weatherAPI.WeatherAPIBL;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import kong.unirest.json.JSONObject;
+import org.apache.log4j.Logger;
 
 public class ApiTestSteps {
     private JSONObject jsonResponse;
+    private static final Logger LOGGER = Logger.getLogger(ApiTestSteps.class.getName());
+    private final TestExecutionContext context;
+    public ApiTestSteps() {
+        context = SessionContext.getTestExecutionContext(Thread.currentThread().getId());
+        LOGGER.info("context: " + context.getTestName());
+    }
 
     @Given("GET request is sent to the weather API with valid latitude {string} and longitude {string}")
     public void getRequestIsSentToTheWeatherAPIWithValidLatitudeAndLongitude(String latitude, String longitude) {
