1cbd2dafc938aa7dc61759c8de1fefe3c49f8ee2
Anand Bagmar <abagmar@gmail.com>
2023-11-22 17:22:57 +0530

    fix issue related to test completion

--- src/main/java/com/znsio/teswiz/runner/AppiumDriverManager.java
+++ src/main/java/com/znsio/teswiz/runner/AppiumDriverManager.java
@@ -315,7 +315,7 @@ class AppiumDriverManager {
     private static void terminateApp(AndroidDriver androidDriver, String appPackageName) {
         Object deviceAPILevel = androidDriver.getCapabilities().getCapability("deviceApiLevel");
         LOGGER.info(String.format("Current API level: '%s'", deviceAPILevel));
-        if (null!=deviceAPILevel && deviceAPILevel.toString().equals("33")) {
+        if (null!=deviceAPILevel && deviceAPILevel.toString().equals("33") && !Runner.getCloudName().equalsIgnoreCase("browserstack")) {
             androidDriver.quit();
         } else {
             androidDriver.terminateApp(appPackageName);
