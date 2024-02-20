dcd73f3679e4e277cea384bd8d197a7c45eef5f6
Anand Bagmar <abagmar@gmail.com>
2024-02-20 12:04:05 +0530

    reverted BrowserStackDevice

--- src/main/java/com/znsio/teswiz/runner/BrowserStackDevice.java
+++ src/main/java/com/znsio/teswiz/runner/BrowserStackDevice.java
@@ -14,27 +14,59 @@ class BrowserStackDevice {
     @JsonProperty("real_mobile")
     private boolean real_mobile;
 
+    String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
     String getDevice() {
         return device;
+    }
+
+    void setDevice(String device) {
+        this.device = device;
     }
 
     String getOs() {
         return os;
     }
 
+    void setOs(String os) {
+        this.os = os;
+    }
+
     String getOs_version() {
         return os_version;
+    }
+
+    void setOs_version(String os_version) {
+        this.os_version = os_version;
     }
 
     String getBrowser() {
         return browser;
     }
 
+    void setBrowser(String browser) {
+        this.browser = browser;
+    }
+
     String getBrowserVersion() {
         return browser_version;
     }
 
+    void setBrowserVersion(String browser_version) {
+        this.browser_version = browser_version;
+    }
+
     boolean isRealMobile() {
         return real_mobile;
+    }
+
+    void setRealMobile(boolean real_mobile) {
+        this.real_mobile = real_mobile;
     }
 }
