d1b25527eb32d664dcc867902e2f1bf3ccd047fe
Anand Bagmar <abagmar@gmail.com>
2026-01-23 10:16:43 +0530

    re-added appium-device-farm plugin

---
+++ src/main/java/com/znsio/teswiz/runner/PluginCli.java
@@ -0,0 +1,68 @@
+package com.znsio.teswiz.runner;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PluginCli {
+    private static final Logger LOGGER = LogManager.getLogger(PluginCli.class.getName());
+    private static PluginCli instance;
+    private Plugin plugin;
+
+    public static PluginCli getInstance() {
+        if (instance == null) {
+            PluginCliRequest plugin = new PluginCliRequest();
+            instance = plugin.getCliArgs();
+        }
+        return instance;
+    }
+
+    public String getPlatFormName() {
+        return getPlugin().getDeviceFarm().getPlatform();
+    }
+
+    public Plugin getPlugin() {
+        return plugin;
+    }
+
+    public boolean isCloudExecution() {
+        return getPlugin().getDeviceFarm().getCloud() != null;
+    }
+
+    public String getCloudName() {
+        return PluginCli.getInstance().getPlugin().getDeviceFarm()
+                .getCloud().get("cloudName").textValue();
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class DeviceFarm {
+        private String platform;
+        private String androidDeviceType;
+        private String iosDeviceType;
+        private JsonNode cloud;
+        private boolean skipChromeDownload;
+        private JsonNode derivedDataPath;
+
+        public String getPlatform() {
+            return platform;
+        }
+
+        public JsonNode getCloud() {
+            return cloud;
+        }
+    }
+
+    public static class Plugin {
+        @JsonProperty("device-farm")
+        @JsonAlias("deviceFarm")
+        private DeviceFarm deviceFarm;
+
+        public DeviceFarm getDeviceFarm() {
+            return deviceFarm;
+        }
+    }
+}
