205aaef2d542e9198dc7c6fb33a740d27e178fca
Anand Bagmar <abagmar@gmail.com>
2026-08-05 18:07:28 +0530

    Extract shared Selenium web execution config

--- src/main/java/com/znsio/teswiz/web/provider/selenium/BrowserStackWebSetup.java
+++ src/main/java/com/znsio/teswiz/web/provider/selenium/BrowserStackWebSetup.java
@@ -4,28 +4,28 @@ import java.util.Map;
 
 import org.openqa.selenium.MutableCapabilities;
 
-import com.znsio.teswiz.runner.Setup;
-
 public final class BrowserStackWebSetup {
-    private static final SeleniumWebSessionNameResolver SESSION_NAME_RESOLVER = new SeleniumWebSessionNameResolver();
     private static final SeleniumWebPlatformCapabilityLoader PLATFORM_CAPABILITY_LOADER =
             new SeleniumWebPlatformCapabilityLoader();
+    private static final SeleniumWebExecutionConfigResolver EXECUTION_CONFIG_RESOLVER =
+            new SeleniumWebExecutionConfigResolver();
 
     private BrowserStackWebSetup() {
     }
 
     public static MutableCapabilities updateCapabilities(MutableCapabilities capabilities) {
         Map<String, Object> loadedPlatformCapability = PLATFORM_CAPABILITY_LOADER.load();
+        SeleniumWebExecutionConfig executionConfig = EXECUTION_CONFIG_RESOLVER.resolve();
         return BrowserStackWebCapabilitySetup.updateBrowserStackCapabilities(
                 capabilities,
                 loadedPlatformCapability,
-                Setup.getFromConfigs(Setup.APP_NAME),
-                Setup.getFromConfigs(Setup.LAUNCH_NAME),
-                Setup.getFromConfigs(Setup.LOG_DIR),
-                SESSION_NAME_RESOLVER.resolve(),
-                Setup.getBooleanValueFromConfigs(Setup.CLOUD_USE_LOCAL_TESTING),
-                Setup.getBooleanValueFromConfigs(Setup.CLOUD_USE_PROXY),
-                Setup.getFromConfigs(Setup.PROXY_URL),
-                Setup.getFromConfigs(Setup.CLOUD_KEY));
+                executionConfig.appName(),
+                executionConfig.launchName(),
+                executionConfig.logDir(),
+                executionConfig.sessionName(),
+                executionConfig.cloudUseLocalTesting(),
+                executionConfig.cloudUseProxy(),
+                executionConfig.proxyUrl(),
+                executionConfig.cloudKey());
     }
 }
--- src/main/java/com/znsio/teswiz/web/provider/selenium/LambdaTestWebSetup.java
+++ src/main/java/com/znsio/teswiz/web/provider/selenium/LambdaTestWebSetup.java
@@ -4,27 +4,27 @@ import java.util.Map;
 
 import org.openqa.selenium.MutableCapabilities;
 
-import com.znsio.teswiz.runner.Setup;
-
 public final class LambdaTestWebSetup {
-    private static final SeleniumWebSessionNameResolver SESSION_NAME_RESOLVER = new SeleniumWebSessionNameResolver();
     private static final SeleniumWebPlatformCapabilityLoader PLATFORM_CAPABILITY_LOADER =
             new SeleniumWebPlatformCapabilityLoader();
+    private static final SeleniumWebExecutionConfigResolver EXECUTION_CONFIG_RESOLVER =
+            new SeleniumWebExecutionConfigResolver();
 
     private LambdaTestWebSetup() {
     }
 
     public static MutableCapabilities updateCapabilities(MutableCapabilities capabilities) {
         Map loadedPlatformCapability = PLATFORM_CAPABILITY_LOADER.load();
+        SeleniumWebExecutionConfig executionConfig = EXECUTION_CONFIG_RESOLVER.resolve();
         return LambdaTestWebCapabilitySetup.updateLambdaTestCapabilities(
                 capabilities,
                 loadedPlatformCapability,
-                Setup.getFromConfigs(Setup.CLOUD_USERNAME),
-                Setup.getFromConfigs(Setup.CLOUD_KEY),
-                Setup.getFromConfigs(Setup.APP_NAME),
-                Setup.getFromConfigs(Setup.LAUNCH_NAME),
-                Setup.getFromConfigs(Setup.LOG_DIR),
-                SESSION_NAME_RESOLVER.resolve(),
-                Setup.getBooleanValueFromConfigs(Setup.CLOUD_USE_LOCAL_TESTING));
+                executionConfig.cloudUser(),
+                executionConfig.cloudKey(),
+                executionConfig.appName(),
+                executionConfig.launchName(),
+                executionConfig.logDir(),
+                executionConfig.sessionName(),
+                executionConfig.cloudUseLocalTesting());
     }
 }
